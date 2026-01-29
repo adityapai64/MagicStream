39c6b6f91610c4d00d6fa07b384993b098e47afe
aditya <adityapai64@gmail.com>
2026-01-29 19:48:55 +0100

    Create endpoint and functionality to add a new movie; add JSON objects for testing

--- Server/MagicStreamServer/controllers/movie_controller.go
+++ Server/MagicStreamServer/controllers/movie_controller.go
@@ -10,9 +10,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
+	"github.com/go-playground/validator/v10"
 )
 
 var movieCollection *mongo.Collection = database.OpenCollection("movies")
+var validate = validator.New()
 
 /*
 ** Function names which begin with capital letters are public. If the first letter
@@ -61,5 +63,64 @@ func GetMovie() gin.HandlerFunc {
 		}
 
 		c.JSON(http.StatusOK, movie)
+	}
+}
+
+func AddMovie() gin.HandlerFunc{
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movie models.Movie
+
+		/*
+			What's happening in the code below:
+
+			- the parameter c holds the context of the http request (line 68)
+			- the method c.shouldBindJSON(&movie) tries to map the JSON object sent 
+			through the request to the movie variable, declared on line 72
+			- If the mapping succeeds, the method returns null (or nil in go);
+			if the mapping throws an error, the error object will be defined
+			- Line 86 simply returns an http 400 error with an error message.
+			- The & character (line 86) is the address of operator in go: it gets
+			the memory address of the variable movie, i.e. it passes a pointer to 
+			the variable movie and not its value
+		*/
+
+		if err:= c.ShouldBindJSON(&movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+			return
+		}
+		/*
+			Validation:
+
+			- Validation is done using the go-playground/validator library
+			- A struct's attrubutes are bound to validation constraints using back ticks (Check
+			the structs in movie_model.go) 
+		*/
+		if err:= validate.Struct(movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Validation failed", "details": err.Error()})
+			return
+		}
+		
+		/*
+		*The code below finally inserts an object (a movie into the database)
+		* - movieCollection is a MongoDB collection object that contains all 
+		* 	movie data.
+		* - The insertOne function inserts one document into the collection
+		* - The first parameter of insertOne, ctx, is the context used for 
+		*	timeout, cancellation or passing metadata
+		* - Movie is the Go Struct that will be converted into a BSON
+		*	document and inserted into the collection
+		*/
+
+		result, err := movieCollection.InsertOne(ctx, movie)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to add movie"})
+			return 
+		}
+
+		c.JSON(http.StatusCreated, result)
 	}
 }
--- Server/MagicStreamServer/models/movie_model.go
+++ Server/MagicStreamServer/models/movie_model.go
@@ -10,7 +10,7 @@ import (
  */
 type Genre struct {
 	GenreID   int    `bson:"genre_id" json:"genre_id" validate:"required"`
-	GenreName string `bson:"genre_name" json:"genre_name" validate:"required, min=2, max=100"`
+	GenreName string `bson:"genre_name" json:"genre_name" validate:"required,min=2,max=100"`
 }
 
 type Ranking struct {
@@ -21,8 +21,8 @@ type Ranking struct {
 type Movie struct {
 	ID         bson.ObjectID `bson:"_id" json:"_id"`
 	ImdbID     string        `bson:"imdb_id" json:"imdb_id" validate:"required"`
-	Title      string        `bson:"title" json:"title" validate:"required, min=2, max=500"`
-	PosterPath string        `bson:"poster_path" json:"poster_path" validate:"required, url"`
+	Title      string        `bson:"title" json:"title" validate:"required,min=2,max=500"`
+	PosterPath string        `bson:"poster_path" json:"poster_path" validate:"required,url"`
 	YouTubeId  string        `bson:"youtube_id" json:"youtube_id" validate:"required"`
 	//The 'dive' keyword below makes sure that the validation is applied to every element of the array.
 	Genre       []Genre `bson:"genre" json:"genre" validate:"required,dive"`
