bd795e3d83d1834d4da131531124e84cfeabd5d2
aditya <adityapai64@gmail.com>
2026-01-30 00:06:27 +0100

    Implement endpoint, model and functionality to create new user

--- Server/MagicStreamServer/models/movie_model.go
+++ Server/MagicStreamServer/models/movie_model.go
@@ -9,7 +9,7 @@ import (
 ** from bson to json as well as for validation.
  */
 type Genre struct {
-	GenreID   int    `bson:"genre_id" json:"genre_id" validate:"required"`
+	GenreID   int    `bson:"genre_id,omitempty" json:"genre_id,omitempty" validate:"required"`
 	GenreName string `bson:"genre_name" json:"genre_name" validate:"required,min=2,max=100"`
 }
 
@@ -19,13 +19,13 @@ type Ranking struct {
 }
 
 type Movie struct {
-	ID         bson.ObjectID `bson:"_id" json:"_id"`
+	ID         bson.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
 	ImdbID     string        `bson:"imdb_id" json:"imdb_id" validate:"required"`
 	Title      string        `bson:"title" json:"title" validate:"required,min=2,max=500"`
 	PosterPath string        `bson:"poster_path" json:"poster_path" validate:"required,url"`
 	YouTubeId  string        `bson:"youtube_id" json:"youtube_id" validate:"required"`
 	//The 'dive' keyword below makes sure that the validation is applied to every element of the array.
 	Genre       []Genre `bson:"genre" json:"genre" validate:"required,dive"`
-	AdminReview string  `bson:"admin_review" json:"admin_review" validate:"required"`
+	AdminReview string  `bson:"admin_review" json:"admin_review"`
 	Ranking     Ranking `bson:"ranking" json:"ranking" validate:"required"`
 }
